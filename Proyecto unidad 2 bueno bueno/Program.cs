5bd3e716deb8811a3727fe5f206200cf63abdf3d
CESAR OMAR CEBALLOS JAUREGUI <21460140@colima.tecnm.mx>
2025-07-07 10:53:05 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto unidad 2 bueno bueno/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_unidad_2_bueno_bueno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
