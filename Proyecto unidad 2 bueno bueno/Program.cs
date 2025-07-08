9a782e7458809f5a38670e775444aba45e831c04
katia valencia <vkatia079@gmail.com>
2025-07-08 08:39:44 -0600

    temperatura

--- Proyecto unidad 2 bueno bueno/Program.cs
+++ Proyecto unidad 2 bueno bueno/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Proyecto_unidad_2_bueno_bueno
+﻿// Clase que representa el sensor de temperatura 
+class SensorTemperatura
 {
-    internal class Program
+    private string nombre = "Sensor de Temperatura";
+    private double valor;
+    private Random random;
+
+    public SensorTemperatura()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        random = new Random();
+    }
+
+    // Simula la lectura de temperatura entre 15 y 35 °C
+    public void LeerValor()
+    {
+        valor = random.Next(15, 36);
+    }
+
+    public void MostrarValor()
+    {
+        Console.WriteLine($"{nombre}: {valor}°C");
+    }
+
+    public double ObtenerValor()
+    {
+        return valor;
     }
 }
